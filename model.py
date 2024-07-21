50467c14a52670d829b528ead7b1293bc5b38664
ramatich <petermaturwe@gmail.com>
2024-07-21 19:50:08 +0300

    modified the dimensions and made it equal

--- model.py
+++ model.py
@@ -3,51 +3,90 @@ from tensorflow.keras import layers
 
 def build_generator():
     inputs = layers.Input(shape=[256, 256, 3])
-    
-    # Encoder
-    down1 = layers.Conv2D(64, (4, 4), strides=2, padding='same', activation='relu')(inputs)
-    down2 = layers.Conv2D(128, (4, 4), strides=2, padding='same', activation='relu')(down1)
-    
-    # Bottleneck
-    bottleneck = layers.Conv2D(256, (4, 4), strides=2, padding='same', activation='relu')(down2)
-    
-    # Decoder
-    up1 = layers.Conv2DTranspose(128, (4, 4), strides=2, padding='same', activation='relu')(bottleneck)
-    concat1 = layers.Concatenate()([up1, down2])
-    up2 = layers.Conv2DTranspose(64, (4, 4), strides=2, padding='same', activation='relu')(concat1)
-    
-    # Output layer
-    outputs = layers.Conv2D(3, (7, 7), activation='tanh', padding='same')(up2)
-    
-    return tf.keras.Model(inputs, outputs)
+
+    down_stack = [
+        downsample(64, 4, apply_batchnorm=False),  # (bs, 128, 128, 64)
+        downsample(128, 4),  # (bs, 64, 64, 128)
+        downsample(256, 4),  # (bs, 32, 32, 256)
+        downsample(512, 4),  # (bs, 16, 16, 512)
+        downsample(512, 4),  # (bs, 8, 8, 512)
+        downsample(512, 4),  # (bs, 4, 4, 512)
+        downsample(512, 4),  # (bs, 2, 2, 512)
+        downsample(512, 4),  # (bs, 1, 1, 512)
+    ]
+
+    up_stack = [
+        upsample(512, 4, apply_dropout=True),  # (bs, 2, 2, 1024)
+        upsample(512, 4, apply_dropout=True),  # (bs, 4, 4, 1024)
+        upsample(512, 4, apply_dropout=True),  # (bs, 8, 8, 1024)
+        upsample(512, 4),  # (bs, 16, 16, 1024)
+        upsample(256, 4),  # (bs, 32, 32, 512)
+        upsample(128, 4),  # (bs, 64, 64, 256)
+        upsample(64, 4),  # (bs, 128, 128, 128)
+    ]
+
+    initializer = tf.random_normal_initializer(0., 0.02)
+
+    last = layers.Conv2DTranspose(
+        3, 4, strides=2,
+        padding='same',
+        kernel_initializer=initializer,
+        activation='tanh')  # (bs, 256, 256, 3)
+
+    x = inputs
+
+    # Downsampling through the model
+    skips = []
+    for down in down_stack:
+        x = down(x)
+        skips.append(x)
+
+    skips = reversed(skips[:-1])
+
+    # Upsampling and establishing the skip connections
+    for up, skip in zip(up_stack, skips):
+        x = up(x)
+        x = layers.Concatenate()([x, skip])
+
+    x = last(x)
+
+    return tf.keras.Model(inputs=inputs, outputs=x)
 
 def build_discriminator():
-    inputs = layers.Input(shape=[256, 256, 3])
-    target = layers.Input(shape=[256, 256, 3])
-    
-    x = layers.Concatenate()([inputs, target])
-    
-    x = layers.Conv2D(64, (4, 4), strides=2, padding='same', activation='relu')(x)
-    x = layers.Conv2D(128, (4, 4), strides=2, padding='same', activation='relu')(x)
-    
-    x = layers.Conv2D(256, (4, 4), strides=2, padding='same', activation='relu')(x)
-    x = layers.Conv2D(512, (4, 4), strides=1, padding='same', activation='relu')(x)
-    
-    x = layers.Flatten()(x)
-    x = layers.Dense(1, activation='sigmoid')(x)
-    
-    return tf.keras.Model([inputs, target], x)
+    initializer = tf.random_normal_initializer(0., 0.02)
+
+    inp = layers.Input(shape=[256, 256, 3], name='input_image')
+    tar = layers.Input(shape=[256, 256, 3], name='target_image')
+
+    x = layers.concatenate([inp, tar])  # (bs, 256, 256, channels*2)
+
+    down1 = downsample(64, 4, False)(x)  # (bs, 128, 128, 64)
+    down2 = downsample(128, 4)(down1)  # (bs, 64, 64, 128)
+    down3 = downsample(256, 4)(down2)  # (bs, 32, 32, 256)
+
+    zero_pad1 = layers.ZeroPadding2D()(down3)  # (bs, 34, 34, 256)
+    conv = layers.Conv2D(512, 4, strides=1,
+                         kernel_initializer=initializer,
+                         use_bias=False)(zero_pad1)  # (bs, 31, 31, 512)
+
+    batchnorm1 = layers.BatchNormalization()(conv)
+
+    leaky_relu = layers.LeakyReLU()(batchnorm1)
+
+    zero_pad2 = layers.ZeroPadding2D()(leaky_relu)  # (bs, 33, 33, 512)
+
+    last = layers.Conv2D(1, 4, strides=1,
+                         kernel_initializer=initializer)(zero_pad2)  # (bs, 30, 30, 1)
+
+    return tf.keras.Model(inputs=[inp, tar], outputs=last)
 
 def compile_pix2pix(generator, discriminator):
-    discriminator.compile(optimizer='adam', loss='binary_crossentropy')
-    
-    inputs = layers.Input(shape=[256, 256, 3])
-    target = layers.Input(shape=[256, 256, 3])
-    generated = generator(inputs, training=True)
-    
-    disc_output = discriminator([inputs, generated], training=True)
-    
-    pix2pix_model = tf.keras.Model(inputs=[inputs, target], outputs=[disc_output, generated])
-    pix2pix_model.compile(optimizer='adam', loss=['binary_crossentropy', 'mae'])
-    
-    return pix2pix_model
+    gen_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
+    disc_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
+
+    loss_object = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+
+    generator.compile(optimizer=gen_optimizer, loss=loss_object)
+    discriminator.compile(optimizer=disc_optimizer, loss=loss_object)
+
+    return generator, discriminator
--- train.py
+++ train.py
@@ -5,7 +5,6 @@ import matplotlib.pyplot as plt
 
 # Load dummy data
 def load_dummy_data():
-    # Implement loading dummy images from data folder
     num_samples = 100
     img_size = (256, 256, 3)
     inputs = np.random.rand(num_samples, *img_size).astype(np.float32)
@@ -17,15 +16,28 @@ input_images, target_images = load_dummy_data()
 # Create models
 generator = build_generator()
 discriminator = build_discriminator()
-pix2pix_model = compile_pix2pix(generator, discriminator)
+generator, discriminator = compile_pix2pix(generator, discriminator)
 
 # Train the Pix2Pix model
-pix2pix_model.fit(
-    [input_images, target_images],
-    [np.ones((len(input_images), 256, 256, 1)), target_images],
-    epochs=10,
-    batch_size=1
-)
+for epoch in range(10):
+    print(f'Starting epoch {epoch+1}')
+    for input_image, target_image in zip(input_images, target_images):
+        with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+            generated_image = generator(tf.expand_dims(input_image, 0), training=True)
+            real_output = discriminator([tf.expand_dims(input_image, 0), tf.expand_dims(target_image, 0)], training=True)
+            fake_output = discriminator([tf.expand_dims(input_image, 0), generated_image], training=True)
+
+            gen_loss = tf.keras.losses.BinaryCrossentropy(from_logits=True)(tf.ones_like(fake_output), fake_output)
+            disc_loss_real = tf.keras.losses.BinaryCrossentropy(from_logits=True)(tf.ones_like(real_output), real_output)
+            disc_loss_fake = tf.keras.losses.BinaryCrossentropy(from_logits=True)(tf.zeros_like(fake_output), fake_output)
+            disc_loss = disc_loss_real + disc_loss_fake
+
+        gradients_of_generator = gen_tape.gradient(gen_loss, generator.trainable_variables)
+        gradients_of_discriminator = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
+
+        generator.optimizer.apply_gradients(zip(gradients_of_generator, generator.trainable_variables))
+        discriminator.optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
+    print(f'Finished epoch {epoch+1}')
 
 # Save the trained model
-pix2pix_model.save('pix2pix_model.h5')
+generator.save('pix2pix_model.h5')

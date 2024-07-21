25941470d0ed2d4c3c64f2a267ece519905bf659
ramatich <petermaturwe@gmail.com>
2024-07-21 18:00:14 +0300

    added the model code and dummy data

--- scripts/dummy_data.py
+++ scripts/dummy_data.py
@@ -6,10 +6,14 @@ def create_dummy_data(num_samples, img_size=(256, 256, 3)):
     targets = np.random.rand(num_samples, *img_size).astype(np.float32)  # Random images for target
     return inputs, targets
 
-num_samples = 100  # Number of dummy images
+num_samples = 6  # Number of dummy images
 input_images, target_images = create_dummy_data(num_samples)
 
 # Optionally, save a few dummy images to visualize
 for i in range(5):
-    plt.imsave(f'data/dummy_input_{i}.png', input_images[i])
-    plt.imsave(f'data/dummy_target_{i}.png', target_images[i])
+    plt.imsave(f'data/motion.jpg', input_images[i])
+    plt.imsave(f'data/motion.jpg', target_images[i])
+    plt.imsave(f'data/Skop.jpg', target_images[i])
+    plt.imsave(f'data/skop2.jpg', target_images[i])
+    plt.imsave(f'data/skop3.jpg', target_images[i])
+    plt.imsave(f'data/skop4.jpg', target_images[i])

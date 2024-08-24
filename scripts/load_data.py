f8bbfe451740180104aa3c30a4f3a37158c6b8d8
ramatich <petermaturwe@gmail.com>
2024-08-24 21:57:28 +0300

    modified the load data

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -1,8 +1,7 @@
 import os
 import numpy as np
-from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from PIL import Image
 import glob
-from PIL import Image  # Don't forget to import Image from PIL
 
 # Function to get the directory of the current script
 def get_script_dir():
@@ -13,11 +12,18 @@ def load_dataset_data(dataset_dir, img_size=(256, 256)):
     image_paths = glob.glob(os.path.join(dataset_dir, '*.png'))
     images = []
     for path in image_paths:
-        img = Image.open(path).resize(img_size)
+        img = Image.open(path)
+        
+        # Convert grayscale images to RGB
+        if img.mode != 'RGB':
+            img = img.convert('RGB')
+        
+        img = img.resize(img_size)
         img = np.array(img).astype(np.float32) / 255.0  # Normalize images to [0, 1]
         images.append(img)
+    
     return np.array(images), np.array(images)  # Using the same images as inputs and targets
 
 # Load real data
-data_dir = os.path.join(get_script_dir(), '..', 'data', 'dataset')  # Adjust the path if needed
+data_dir = os.path.join(get_script_dir(), '..', 'data', 'dataset')
 input_images, target_images = load_dataset_data(data_dir)
